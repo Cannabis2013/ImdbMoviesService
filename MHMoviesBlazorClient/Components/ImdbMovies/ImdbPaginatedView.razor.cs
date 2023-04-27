1421766f1ed4cd5a7a9ba5121d32a4251a678619
Cannabis2013 <MH2012@outlook.dk>
2023-04-27 13:12:55 +0200

    Added variable button count selector

--- MHMoviesBlazorClient/Components/ImdbMovies/ImdbPaginatedView.razor.cs
+++ MHMoviesBlazorClient/Components/ImdbMovies/ImdbPaginatedView.razor.cs
@@ -5,6 +5,8 @@ namespace MHMoviesBlazorClient.Components.ImdbMovies;
 
 public partial class ImdbPaginatedView : ComponentBase
 {
+    private int _buttonsCount = 5;
+    
     [Inject] private ImdbPageFetch PaginatedFetch { get; set; } = null!;
     
     protected override async Task OnInitializedAsync() => await PaginatedFetch.Initialize();
